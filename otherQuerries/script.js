bcb4e71f9e86b0ba8caab8d201028643e8e90404
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-03-15 21:55:20 -0300

    Add files via upload

---
+++ otherQuerries/script.js
@@ -0,0 +1,8 @@
+// obter um elemento atraves do ID
+const title = document.getElementById('title')
+
+// obter elementos atraves do nome da classe
+const errors = document.getElementsByClassName('error') //nao precisa de ponto
+
+// obter elementos atraves do nome da tag
+const paragraph = document.getElementsByTagName('p') // retorna todas as tags p da pagina
